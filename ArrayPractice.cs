e1f103aa3908ba97eb751f5b08ddb110b18df701
mispeled <gabrieljosephdougherty@gmail.com>
2017-04-15 22:06:15 -0400

    Array Practice

---
+++ ArrayPractice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NUMBER ADDER\n" +
+                "Enter a series of numbers, 'exit' to Quit.");
+            string input = "";
+            int[] nums = new int[256];
+
+            // I could of course sum in this loop, but then why use arrays at all?
+            int i = 0;
+            do
+            {
+                input = Console.ReadLine();
+                int newNum = 0;
+                int.TryParse(input, out newNum);
+                nums[i++] = newNum; // post increment increments `i` after assignment
+            } while (string.Compare(input, "exit", true) != 0); // compares to "exit" and ignores case
+            Console.WriteLine("The produced array has {0} items", i-1);
+
+            int total;
+            for (total = 0, i--; i >= 0; i--)
+            {
+                total += nums[i];
+            }
+            Console.WriteLine("Sum: {0}", total);
+
+            // wait
+            Console.ReadLine();
+        }
+    }
+}
